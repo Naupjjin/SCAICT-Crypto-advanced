fd74e9bba6da62df82cd90eae446f2ca864065cc
naup <naup96721@gmail.com>
2024-06-16 10:19:46 +0800

    ECBscript

--- block cipher/cut-and-paste/solve.py
+++ block cipher/cut-and-paste/solve.py
@@ -1,9 +1,9 @@
 from pwn import *
-#user:KasuganoSor
-#a;padding:aaaaaa
-#aaaaaaaaa;favora
-#bility:90000000;
-#favorability:0;
+#user:KasuganoSor  ->block 1 -> 0~15
+#a;padding:aaaaaa  ->block 2 -> 16~31
+#aaaaaaaaa;favora  ->block 3 -> 32~47
+#bility:90000000;  ->block 4 -> 48~63
+#favorability:0;   ->block 5 -> 64~79
 
 #payload:
 #aaaaaaaaaaaaaaa;favorability:90000000;
@@ -14,7 +14,10 @@ r.sendline(b'aaaaaaaaaaaaaaa;favorability:90000000;')
 r.recvuntil(b'=')
 token=bytes.fromhex(r.recvline().strip().split(b"=")[1].decode())
 print(token)
+
+#0~63
 token=token[:64]
+
 r.sendlineafter('token = ', token.hex())
 r.sendline(b"Y")
 
