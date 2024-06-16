a24d2a994cca90b9cd709d0d97c2e764cb62c21f
naup <naup96721@gmail.com>
2024-06-16 11:25:31 +0800

    a

--- RSA/factorDB/server.py
+++ RSA/factorDB/server.py
@@ -1,19 +1,8 @@
 from Crypto.Util.number import *
 
-def encrypt(m,e,n):
-    c=pow(m,e,n)
-    return c
-
-msg = b"CryptoADV{Fake_Flag}"
-m = bytes_to_long(msg)
-
-p=getPrime(50)
-q=getPrime(50)
+p=getPrime(64)
+q=getPrime(64)
 n=p*q
-e=65537
 
 print("n= ", n)
-print("p= ", p)
-print("q= ", q)
-print("e= ", e)
-print("c= ", encrypt(m,e,n))
+
